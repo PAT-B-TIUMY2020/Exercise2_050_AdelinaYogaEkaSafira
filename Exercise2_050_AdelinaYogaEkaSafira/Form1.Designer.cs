bf7b269fd8547f7ef33cd6f0b256a732f2664443
adelina-sfr <62781888+adelina-sfr@users.noreply.github.com>
2020-12-25 22:36:07 +0700

    Add project files.

---
+++ Exercise2_050_AdelinaYogaEkaSafira/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Exercise2_050_AdelinaYogaEkaSafira
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtMahasiswa = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.textBoxNIM = new System.Windows.Forms.TextBox();
+            this.textBoxProdi = new System.Windows.Forms.TextBox();
+            this.textBoxAngkatan = new System.Windows.Forms.TextBox();
+            this.buttonSimpan = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxByNIM = new System.Windows.Forms.TextBox();
+            this.buttonCari = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonJumlahData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtMahasiswa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtMahasiswa
+            // 
+            this.dtMahasiswa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtMahasiswa.Location = new System.Drawing.Point(0, 280);
+            this.dtMahasiswa.Name = "dtMahasiswa";
+            this.dtMahasiswa.RowTemplate.Height = 24;
+            this.dtMahasiswa.Size = new System.Drawing.Size(795, 167);
+            this.dtMahasiswa.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NIM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Prodi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Angkatan";
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(125, 48);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(178, 22);
+            this.textBoxNama.TabIndex = 5;
+            // 
+            // textBoxNIM
+            // 
+            this.textBoxNIM.Location = new System.Drawing.Point(125, 107);
+            this.textBoxNIM.Name = "textBoxNIM";
+            this.textBoxNIM.Size = new System.Drawing.Size(178, 22);
+            this.textBoxNIM.TabIndex = 6;
+            // 
+            // textBoxProdi
+            // 
+            this.textBoxProdi.Location = new System.Drawing.Point(125, 165);
+            this.textBoxProdi.Name = "textBoxProdi";
+            this.textBoxProdi.Size = new System.Drawing.Size(178, 22);
+            this.textBoxProdi.TabIndex = 7;
+            // 
+            // textBoxAngkatan
+            // 
+            this.textBoxAngkatan.Location = new System.Drawing.Point(125, 228);
+            this.textBoxAngkatan.Name = "textBoxAngkatan";
+            this.textBoxAngkatan.Size = new System.Drawing.Size(178, 22);
+            this.textBoxAngkatan.TabIndex = 8;
+            // 
+            // buttonSimpan
+            // 
+            this.buttonSimpan.Location = new System.Drawing.Point(367, 47);
+            this.buttonSimpan.Name = "buttonSimpan";
+            this.buttonSimpan.Size = new System.Drawing.Size(75, 23);
+            this.buttonSimpan.TabIndex = 9;
+            this.buttonSimpan.Text = "Simpan";
+            this.buttonSimpan.UseVisualStyleBackColor = true;
+            this.buttonSimpan.Click += new System.EventHandler(this.buttonSImpan_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(367, 104);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 10;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(367, 170);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 11;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(367, 225);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 12;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // textBoxByNIM
+            // 
+            this.textBoxByNIM.Location = new System.Drawing.Point(485, 48);
+            this.textBoxByNIM.Name = "textBoxByNIM";
+            this.textBoxByNIM.Size = new System.Drawing.Size(156, 22);
+            this.textBoxByNIM.TabIndex = 13;
+            // 
+            // buttonCari
+            // 
+            this.buttonCari.Location = new System.Drawing.Point(701, 48);
+            this.buttonCari.Name = "buttonCari";
+            this.buttonCari.Size = new System.Drawing.Size(75, 23);
+            this.buttonCari.TabIndex = 14;
+            this.buttonCari.Text = "Cari";
+            this.buttonCari.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(548, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "0";
+            // 
+            // buttonJumlahData
+            // 
+            this.buttonJumlahData.Location = new System.Drawing.Point(657, 110);
+            this.buttonJumlahData.Name = "buttonJumlahData";
+            this.buttonJumlahData.Size = new System.Drawing.Size(119, 23);
+            this.buttonJumlahData.TabIndex = 16;
+            this.buttonJumlahData.Text = "Jumlah Data";
+            this.buttonJumlahData.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonJumlahData);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonCari);
+            this.Controls.Add(this.textBoxByNIM);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonSimpan);
+            this.Controls.Add(this.textBoxAngkatan);
+            this.Controls.Add(this.textBoxProdi);
+            this.Controls.Add(this.textBoxNIM);
+            this.Controls.Add(this.textBoxNama);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtMahasiswa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtMahasiswa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtMahasiswa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.TextBox textBoxNIM;
+        private System.Windows.Forms.TextBox textBoxProdi;
+        private System.Windows.Forms.TextBox textBoxAngkatan;
+        private System.Windows.Forms.Button buttonSimpan;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxByNIM;
+        private System.Windows.Forms.Button buttonCari;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonJumlahData;
+    }
+}
+
